7cfa6a68ec1e51ea727605d4af7fa11c60f78fc9
EKHO <lukasz.ksiazek1350@gmail.com>
2023-10-07 11:22:28 +0200

    AudioConverter Update

--- SectionAudioConverter.py
+++ SectionAudioConverter.py
@@ -1,18 +1,16 @@
 import mido
+from ImageSection import ImageSection as IS
+
+track = mido.MidiTrack()
+mid = mido.MidiFile()
+SectionList = None
 
 class SectionAudioConverter:
-
-    def __init__(self, xPos, yPos, zPos, intensity, trackTempo):
-        self.xPos = xPos
-        self.yPos = yPos
-        self.zPos = zPos
-        self.intensity = intensity
-        self.trackTempo = trackTempo
-        track = mido.MidiTrack()
-        mid = mido.MidiFile()
+    def __init__(self, IS, trackTempo):
+        SectionsList = IS.GetSections()
         mid.tracks.append(track)
         track.append(mido.MetaMessage('set_tempo', tempo=trackTempo))
 
-
     def ToMidi(self):
         program_change = mido.Message('program_change', program=27, channel=0)
+        track.append(program_change)
