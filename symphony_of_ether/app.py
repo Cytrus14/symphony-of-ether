54269379a7bdc7b62c728e77c8dde04f3adabd65
SebeK2221 <144722562+SebeK2221@users.noreply.github.com>
2023-10-07 22:47:28 +0200

    logika do zdjęcia
    l

--- symphony_of_ether/app.py
+++ symphony_of_ether/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__, template_folder='templates')
 
+# Przechowuje współrzędne
+coordinates = {'x': None, 'y': None}
 
 @app.route('/')
 def index():
@@ -10,10 +12,31 @@ def index():
 @app.route('/about')
 def about():
     return render_template('base.html')
+
 @app.route('/video')
 def video():
     video_files = ['video1.mp4']
-    return render_template('video.html',video_files=video_files)
+    return render_template('video.html', video_files=video_files)
+
+@app.route('/send-coordinates', methods=['POST'])
+def receive_coordinates():
+    try:
+        data = request.get_json()
+        x = data['x']
+        y = data['y']
+
+        # Zapisz współrzędne w słowniku
+        coordinates['x'] = x
+        coordinates['y'] = y
+
+        # Wypisz współrzędne na konsoli
+        print(f'Odebrane współrzędne - X: {x}, Y: {y}')
+
+        # Możesz tutaj wykonać operacje na otrzymanych współrzędnych, jeśli jest to potrzebne
+
+        return jsonify({'message': 'Dane współrzędnych zostały zapisane pomyślnie'})
+    except Exception as e:
+        return jsonify({'error': 'Wystąpił błąd podczas przetwarzania danych'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
