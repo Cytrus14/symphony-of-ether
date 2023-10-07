89c3b6ee16a8aeafc395bfb4cb01f96eeb427b52
SebeK2221 <144722562+SebeK2221@users.noreply.github.com>
2023-10-07 20:47:37 +0200

    karta wideo
    dodana karta wideo

--- symphony_of_ether/app.py
+++ symphony_of_ether/app.py
@@ -10,6 +10,10 @@ def index():
 @app.route('/about')
 def about():
     return render_template('base.html')
+@app.route('/video')
+def video():
+    video_files = ['video1.mp4']
+    return render_template('video.html',video_files=video_files)
 
 if __name__ == '__main__':
     app.run(debug=True)
