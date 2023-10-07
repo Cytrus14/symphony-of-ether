c61424e1ec06fd1331f67ac0a246149b13d3616f
Cytrus <dominic_mat@tutanota.com>
2023-10-07 10:44:36 +0200

    Added id field to ImageSection

--- ImageSection.py
+++ ImageSection.py
@@ -1,5 +1,8 @@
+import uuid
+
 class ImageSection:
     def __init__(self, x_pos=0, y_pos=0, z_pos=0, intensity=0):
+        self.id = int(str(uuid.uuid4()).replace('-', ''), 16)
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.z_pos = z_pos
