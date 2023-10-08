fe665c53c342d6b1d11c7d7843f3d2fe32a80cb4
EKHO <lukasz.ksiazek1350@gmail.com>
2023-10-08 02:21:02 +0200

    Audio Converter v3

--- SectionAudioConverter.py
+++ SectionAudioConverter.py
@@ -9,7 +9,7 @@ soundFilePath = "Sounds/ImageMusic.wav"
 
 
 class SectionAudioConverter:
-    def __init__(self, chanelCount = 1):
+    def __init__(self, chanelCount=1):
         self.volume = 64
         self.trackTempo = 1000
         self.trackInstrumental = mido.MidiTrack()
@@ -18,18 +18,17 @@ class SectionAudioConverter:
         self.mid.tracks.append(self.trackInstrumental)
         self.trackInstrumental.append(mido.MetaMessage('set_tempo', tempo=self.trackTempo))
 
-
         self.IS = ImageSelectionSelector("m31", 4, 0, 0)
         self.sps = 44100
         self.freq_hz = 220.0
         self.duration = 0.1
-        self.vol = 0.01
+        self.vol = 1
         self.esm = np.arange(self.duration * self.sps)
 
         self.trackSynth = AudioSegment.empty()
 
         self.chanelCount = chanelCount + 1
-        self.VM = VideoMaker(1/self.duration)
+        self.VM = VideoMaker(1 / self.duration)
 
     def InstrumentalConvert(self):
         secList = self.IS.get_sections()
@@ -41,8 +40,10 @@ class SectionAudioConverter:
                     distanceFromCenter = 200
                 program_change = mido.Message('program_change', program=section.z_pos, channel=0)
                 self.trackInstrumental.append(program_change)
-                self.trackInstrumental.append(mido.Message('note_on', note=section.intensity%5, velocity=self.volume, time=0))
-                self.trackInstrumental.append(mido.Message('note_off', note=section.intensity%5, velocity=self.volume, time=1))
+                self.trackInstrumental.append(
+                    mido.Message('note_on', note=section.intensity % 5, velocity=self.volume, time=0))
+                self.trackInstrumental.append(
+                    mido.Message('note_off', note=section.intensity % 5, velocity=self.volume, time=1))
             '''
             for section in secList:
                 distanceFromCenter = math.sqrt(math.pow(section.x_pos, 2) + math.pow(section.y_pos, 2))
@@ -53,7 +54,7 @@ class SectionAudioConverter:
         self.mid.save(soundFilePath)
 
     def fun(self, x, a):
-        return np.log(a * np.sin(x))
+        return a * np.sin(x)
 
     def SynthConvert(self):
 
@@ -70,9 +71,10 @@ class SectionAudioConverter:
                     dominantIntensity += math.pow(1.05, section.intensity)
                 dominantIntensity /= len(ListOfSect)
 
-                currentFreq = self.freq_hz/chanel
+                currentFreq = self.freq_hz + chanel * dominantIntensity
+                currentVol = self.vol
                 waveFunction = self.fun((2 * np.pi * self.esm * currentFreq / self.sps), dominantIntensity)
-                waveFunctionQuiet = waveFunction * self.vol
+                waveFunctionQuiet = waveFunction * currentVol
                 waveFunctionInt = np.int16(waveFunctionQuiet * 32767)
 
                 new_segment = AudioSegment(
@@ -92,10 +94,9 @@ class SectionAudioConverter:
         finalTrack.export(soundFilePath, format="wav")
 
 
-
-
 SAC = SectionAudioConverter(4)
 SAC.SynthConvert()
+
 VM = VideoMaker(fps=10)
 VM.audio_path = "Sounds/ImageMusic.wav"
 VM.gen_video("combined")
